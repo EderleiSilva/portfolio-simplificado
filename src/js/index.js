ad0c10bbd9b48b38a2d87a8528b6338b28957849
Ederlei Silva <ederleipereiras1@hotmail.com>
2024-12-09 10:14:26 -0300

    Configurado o botão download com JavaScript

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
-const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
-const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+const botaoMostrarProjetos = document.querySelector(".btn-mostrar-projetos");
+const projetosInativos = document.querySelectorAll(".projeto:not(.ativo)");
+
+const botaoDownload = document.getElementById("btnDownload");
 
 botaoMostrarProjetos.addEventListener("click", () => {
   mostrarMaisProjetos();
@@ -14,6 +16,14 @@ function esconderBotao() {
 
 function mostrarMaisProjetos() {
   projetosInativos.forEach(projetoInativo => {
-    projetoInativo.classList.add('ativo');
+    projetoInativo.classList.add("ativo");
   });
 }
+
+botaoDownload.addEventListener("click", () => {
+  const link = document.createElement("a");
+  link.target = "_blank";
+  link.href = "/src/assets/curriculo/Ederlei_Pereira_dos_Santos_Silva.pdf";
+  link.download = "Ederlei_Pereira_dos_Santos_Silva.pdf";
+  link.click();
+});
