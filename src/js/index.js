c95fcb1171a0f71e4926ce4ba465e8ec4bf37da5
Ederlei Silva <ederleipereiras1@hotmail.com>
2024-12-09 16:38:03 -0300

    Verificado codigo js

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ const botaoDownload = document.getElementById("btnDownload");
 botaoDownload.addEventListener("click", () => {
   const link = document.createElement("a");
   link.target = "_blank";
-  link.href = "/src/assets/curriculo/Ederlei_Pereira_dos_Santos_Silva.pdf";
+  link.href = "./src/assets/curriculo/Ederlei_Pereira_dos_Santos_Silva.pdf";
   link.download = "Ederlei_Pereira_dos_Santos_Silva.pdf";
   link.click();
 });
