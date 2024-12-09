8078d66b30ad3d454cae8c0c984e58eed7475cce
Ederlei Silva <ederleipereiras1@hotmail.com>
2024-12-09 17:03:57 -0300

    Curriculo adicionado com link direto e retirado codigo JS do portifólio

--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-const botaoDownload = document.getElementById("btnDownload");
-
-botaoDownload.addEventListener("click", () => {
-  const link = document.createElement("a");
-  link.target = "_blank";
-  link.href = "./src/assets/curriculo/Ederlei_Pereira_dos_Santos_Silva.pdf";
-  link.download = "Ederlei_Pereira_dos_Santos_Silva.pdf";
-  link.click();
-});
