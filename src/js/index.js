dee9c26ec7ca2a94c2f0e78298901de4b21dd5b8
Ederlei Silva <ederleipereiras1@hotmail.com>
2025-01-18 10:27:52 -0300

    Adicionado zoom aos certificados para poder ler

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+const certificados = document.querySelectorAll(".certificado");
+const tituloCertificado = document.querySelectorAll(".tituloCertificado");
+
+certificados.forEach(certificado => {
+    certificado.addEventListener('click', () => {
+        const classZoomExiste = document.querySelector(".zoom");
+
+        if(certificado.classList.contains("zoom")) {
+            certificado.classList.remove("zoom");
+        } else {
+            certificado.classList.add("zoom");
+        }
+
+        if(classZoomExiste){
+            classZoomExiste.classList.remove("zoom");
+        }
+    });
+});
