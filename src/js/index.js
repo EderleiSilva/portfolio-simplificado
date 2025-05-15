a49b6ab7eba281d6d9e8cb38fb781cf3b03c92a7
Ederlei Silva <ederleipereiras1@hotmail.com>
2025-05-14 21:30:18 -0300

    Adicionado novas habilidades e novo projeto no portifolio

--- src/js/index.js
+++ src/js/index.js
@@ -5,14 +5,26 @@ certificados.forEach(certificado => {
     certificado.addEventListener('click', () => {
         const classZoomExiste = document.querySelector(".zoom");
 
-        if(certificado.classList.contains("zoom")) {
+        if (certificado.classList.contains("zoom")) {
             certificado.classList.remove("zoom");
         } else {
             certificado.classList.add("zoom");
         }
 
-        if(classZoomExiste){
+        if (classZoomExiste) {
             classZoomExiste.classList.remove("zoom");
         }
     });
 });
+
+certificados.forEach(certificado => {
+    certificado.addEventListener('click', () => {
+        tituloCertificado.forEach(titulo => {
+            if (certificado.classList.contains("zoom")) {
+                titulo.classList.add("disable");
+            } else {
+                titulo.classList.remove("disable");
+            }
+        });
+    });
+})
